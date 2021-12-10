5219efcb99586d169f6da9f7410cb9add3c0b906
Shamir Gibbons <nonshalant@shamirs-air.myfiosgateway.com>
2021-12-10 14:48:35 -0500

    finished w project;

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,18 +1,32 @@
 // Write your Character component here
-import styled from "styled-components";
 import react from "react";
+import styled from "styled-components";
 
+const StyledCharacter = styled.div`
+color: red;
+font-weight: bold;
+font-size: 2rem;
+background-color: black;
+height: 100%;
+width: 90%;
+margin: auto;
+border-radius: 10%;
+border: 1px solid red;
 
+&:hover{
+    display:none;
+}
+`
 
 const Character = (props) =>{
     const {data} = props;
     
     return (
-    <div>
+    <StyledCharacter>
       {data.map( char => {
           return char.name
       })}
-    </div>
+    </StyledCharacter>
     )
 }
 
