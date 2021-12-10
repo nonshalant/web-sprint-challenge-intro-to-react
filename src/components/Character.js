b89fe384c07275f8288450414d8d6fed2c676f32
Shamir Gibbons <nonshalant@shamirs-air.myfiosgateway.com>
2021-12-10 12:56:43 -0500

    each char from the char array to console.log

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,16 @@
 // Write your Character component here
+
+import react from "react";
+
+const Character = (props) =>{
+    const {data} = props;
+    
+    return (
+        data.map((char, index)=>{
+            console.log(char)
+        })
+        
+    )
+}
+
+export default Character;
